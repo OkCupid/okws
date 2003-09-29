61739b30d86d3f26368dc96a56ca301190082b3d
len <len@7d287422-6eea-da11-b677-00123f2a783a>
2003-09-29 17:37:36 +0000

    added the affected_rows method
    git-svn-id: svn://okws.org/trunk/okws2@33 7d287422-6eea-da11-b677-00123f2a783a

--- libamysql/amysql.h
+++ libamysql/amysql.h
@@ -444,6 +444,7 @@ public:
   str error () const { return err; }
   ptr<mystmt_t> prepare (const str &q, u_int opts = AMYSQL_DEFAULT);
   u_int64_t insert_id () { return mysql_insert_id (&mysql); }
+  u_int64_t affected_rows() { return mysql_affected_rows(&mysql);}
 
   u_int opts;
   str err;
