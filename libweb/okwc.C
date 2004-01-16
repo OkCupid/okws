10fe5892aa42b8549a6d6f6aa21d81a26973f88e
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2004-01-16 17:36:54 +0000

    made small progress on this
    git-svn-id: svn://okws.org/trunk/okws2@218 7d287422-6eea-da11-b677-00123f2a783a

--- libweb/okwc.C
+++ libweb/okwc.C
@@ -1,1 +1,107 @@
 
+#include "okwc.h"
+#include "httpconst.h"
+
+okwc_req_t::okwc_req_t (const str &h, u_int16_t p, const str &f, cgi_t *inc,
+			okwc_cb_t c, int to)
+  : hostname (h), port (p), filename (f),
+    incookie (inc), okwc_cb (c), timeout (to),
+    tcpcon (NULL), fd (-1), http (NULL), timer (NULL) {}
+
+void
+okwc_req_t::launch ()
+{
+  tcpcon = tcpconnect (hostname, port, wrap (this, &okwc_req_t::tcpcon_cb));
+  if (timeout > 0) 
+    timer = delaycb (timeout, 0, wrap (this, &okwc_req_t::timeout_cb));
+}
+
+void
+okwc_req_t::tcpcon_cb (int f)
+{
+  tcpcon = NULL;
+
+  if (f < 0) {
+    finish (HTTP_CONNECTION_FAILED);
+    return;
+  }
+  fd = f;
+  x = ahttpcon::alloc (fd);
+  http = New okwc_http_t (x, filename, wrap (this, &okwc_req_t::http_cb));
+  http->make_req ();
+}
+
+void
+okcw_req_t::http_cb (ptr<okwc_resp_t> res)
+{
+  (*okwc_cb) (res);
+  delete this;
+}
+
+void
+okwc_req_t::timeout_cb ()
+{
+  if (tcpcon) {
+    tcpconnect_cancel (tcpcon);
+    tcpcon = NULL;
+    assert (!http);
+  } else {
+    http->cancel ();
+  }
+  finish (HTTP_TIMEOUT);
+
+}
+
+void
+okwc_req_t::finish (int status)
+{
+  (*okwc_cb) (okwc_resp_t::alloc (status));
+  delete this;
+}
+
+okwc_req_t::~okwc_req_t ()
+{
+  if (http)
+    delete http;
+  if (timer) {
+    timecb_remove (timer);
+    timer = NULL;
+  }
+}
+
+
+void
+okwc_http_t::make_req ()
+{
+  reqbuf << "GET " << filename << " HTTP/1.0" << HTTP_CRLF;
+  if (cookie) {
+    reqbuf << "Cookie: ";
+    cookie->encode (&reqbuf);
+    reqbuf << HTTP_CRLF;
+  }
+  reqbuf << HTTP_CRLF;
+  x->send (reqbuf, wrap (this, &okwc_http_t::parse));
+}
+
+void
+okwc_http_t::parse ()
+{
+  resp = okwc_resp_t::alloc (HTTP_OK);
+  hdr.parse (resp, wrap (this, &okwc_http_t::hdr_parsed));
+}
+
+void
+okwc_http_t::hdr_parsed (int status)
+{
+  if (status != HTTP_OK) {
+    finish (status);
+  } else {
+    body.parse (resp, hdr.contlen, wrap (this, &okwc_http_t::body_parsed));
+  }
+}
+
+void
+okwc_http_t::body_parsed (int status)
+{
+  finish (status);
+}
--- libweb/okwc.h
+++ libweb/okwc.h
@@ -14,45 +14,63 @@
 // okwc = OK Web Client
 
 struct okwc_resp_t {
-  okwc_resp_t () {}
+  okwc_resp_t (int s) : status (s) {}
+
+  static ptr<okwd_resp_t> alloc (int s) 
+  { return New refcounted<okwc_resp_t> (s); }
+
   str body;
   int status;
   cgi_t cookie;
 };
 
-typedef callback<void, ptr<okwc_resp_t> > okwc_cb_t;
+typedef callback<void, ptr<okwc_resp_t> >::ref okwc_cb_t;
 
 #define OKWC_SCRATCH_SZ 4096
 
 //
 // application level HTTP object
 //
-struct okwc_http_t {
-  okwc_http_t (ptr<ahttpcon> xx, const str &f, int to, cbv c);
+struct okwc_http_t : public virtual async_parser_t {
+  okwc_http_t (ptr<ahttpcon> xx, const str &f, int to, okwc_cb_t c);
+
+  void make_req ();
+  void cancel ();
 
   ptr<ahttpcon> x;
   str filename;
   abuf_t abuf;
-  http_hdr_t hdr;
+
+  okwc_http_hdr_t hdr;
+  okwc_http_body_t body;
   int timeout;
-  cbv cb;
 
   char scratch[OKWC_SCRATCH_SZ];
   ptr<okwc_resp_t> resp;
 };
 
-struct okwc_req_t {
-  okwc_req_t (const str &h, int p, const str &fn, cgi_t *incook,
+class okwc_req_t {
+public:
+  okwc_req_t (const str &h, u_int16_t p, const str &fn, cgi_t *incook,
 	      okwc_cb_t cb, int timeout);
+  ~okwc_req_t ();
+
+  void launch ();
+
+private:
+  void tcpcon_cb (int fd);
+  void timeout_cb ();
+  void finish ();
 
   const str hostname;
-  const int port;
+  const u_int16_t port;
   const str filename;
   cgi_t *incookie;
   okwc_cb_t okwc_cb;
   int timeout;
 
-  tcpconnect_t *tcpchan;
+  tcpconnect_t *tcpcon;
+
   int fd;
   ptr<ahttpcon> x;
   okwc_http_t *http;
