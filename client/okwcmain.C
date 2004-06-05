299cb2e6bb924dbcaf9170cfacccbf6b7f8f957f
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2004-06-05 03:01:34 +0000

    okwc now supports HTTP/1.1
    git-svn-id: svn://okws.org/trunk/okws2@468 7d287422-6eea-da11-b677-00123f2a783a

--- client/okwcmain.C
+++ client/okwcmain.C
@@ -44,7 +44,7 @@ main (int argc, char *argv [])
     assert (convertint (port_str, &port));
   str filename = url_rxx[4];
 
-  okwc_request (hostname, port, filename, wrap (reqcb), 100);
+  okwc_request (hostname, port, filename, wrap (reqcb), 1, 100);
   amain ();
 }
 
