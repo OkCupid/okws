3c1697ea06f9d09f71715cdcb5014ed4ae27383b
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2004-06-06 13:34:46 +0000

    a little program for benchmarking virtual function calls.
    git-svn-id: svn://okws.org/trunk/okws2@472 7d287422-6eea-da11-b677-00123f2a783a

---
+++ client/bench.C
@@ -0,0 +1,43 @@
+
+#include "async.h"
+
+class A {
+public:
+  A () : v (0) {}
+  virtual ~A () {}
+  virtual foo () = 0;
+protected:
+  u_int64_t v;
+};
+
+class B : public A {
+public:
+  B () : A () {}
+  void foo () { v++; }
+};
+
+
+
+timeval tv;
+
+inline void startt () { gettimeofday (&tv, NULL); }
+inline int stopt () 
+{ 
+  timeval tv2; 
+  gettimeofday (&tv2, NULL); 
+  return ( (tv2.tv_sec - tv.tv_sec) * 1000000 +
+	   (tv2.tv_usec - tv.tv_usec)) ;
+}
+
+int
+main (int argc, char *argv[])
+{
+  B b;
+  startt ();
+  int iter = 1000000;
+  for (int i = 0; i < iter; i++) {
+    b.foo ();
+  }
+  int t = stopt ();
+  warn ("%d iter in %d usec: %g\n", iter, t, (double)t / (double)iter);
+}
