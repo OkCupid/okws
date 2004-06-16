9436bfd568371b2291d1e80827f7a95d0bf55847
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2004-06-16 13:36:53 +0000

    checked in some debug code.
    git-svn-id: svn://okws.org/trunk/okws2@500 7d287422-6eea-da11-b677-00123f2a783a

--- libpub/zstr.C
+++ libpub/zstr.C
@@ -225,10 +225,17 @@ zbuf::compress (strbuf *p, int level)
   str s;
   u_int len;
 
+  // XXX -- debug code
+  vec<int> debugvec;
+
   // first time through the loop add the gzip header; see zinit ()
   // for what that header actually looks like
   for (u_int i = 0; i < lim; i++) {
     str z = zs[i].compress (level);
+    
+    // XXX -- debug code
+    debugvec.push_back (i);
+
     if (!z)
       goto compress_err;
     len = (i == 0) ? zhdr.len () + z.len () : z.len ();
@@ -260,6 +267,12 @@ zbuf::compress (strbuf *p, int level)
   s = endbuf;
   (*p) << s << "\r\n0\r\n\r\n";
   return;
+
+  // XXX - debug code
+  for (u_int i = 0; i < debugvec.size (); i++) {
+    warn << debugvec[i] << ",";
+  }
+  warn << "\n";
 	  
  compress_err:
   p->tosuio ()->clear ();
