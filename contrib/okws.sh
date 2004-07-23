1865ade8562975ab751201f48ed66e9c07955f50
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2004-07-23 20:29:58 +0000

    rc.d script for starting OKWS
    git-svn-id: svn://okws.org/trunk/okws2@583 7d287422-6eea-da11-b677-00123f2a783a

---
+++ contrib/okws.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#
+# OKWS rc.d control script
+#
+# This is a standard FreeBSD 5.X rc.d script to control okws.  It 
+# responds to all standard rc.d commands:
+#
+#   Usage: okld.sh [fast|force](start|stop|restart|rcvar|status|poll)
+#
+# In order for it to work, okws_enable="YES" needs to be set in 
+# /etc/rc.conf.  A good place for this script would be 
+# /usr/local/etc/rc.d.
+#
+# I don't recommend using 'fast'.
+#
+# Author:  Patrick Crosby <patrick@okcupid.com>
+#
+# $Id$
+#
+
+. /etc/rc.subr
+
+name="okws"
+rcvar=`set_rcvar`
+command="/usr/local/sbin/okld"
+okws_flags="-q"
+pidfile="/var/run/okld.pid"
+required_files="/etc/sfs/okws_config"
+extra_command="reload"
+
+load_rc_config $name
+run_rc_command "$1"
+
