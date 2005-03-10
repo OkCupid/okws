00e5369e51c2220fc376819a43e170584910e134
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2005-03-10 01:32:55 +0000

    oops, forgot these.
    git-svn-id: svn://okws.org/trunk/okws2@706 7d287422-6eea-da11-b677-00123f2a783a

---
+++ client/okdbg.C
@@ -0,0 +1,47 @@
+
+
+#include "okdbg.h"
+
+static void
+usage ()
+{
+  warnx << "usage: " << progname  << " [-abB]\n"
+	<< "\t -b pub subsystem: dump bind table after each insert\n"
+	<< "\t -a pub subsystem: dump bind table after all inserts\n"
+	<< "\t -B pub subsystem: dump bind table before access\n"
+	<< "\t -E pub subsystem: complain about errors\n"
+    ;
+  exit (1);
+}
+
+
+int
+main (int argc, char *argv[])
+{
+  int ch;
+  int64_t res = 0;
+setprogname (argv[0]);
+  while ((ch = getopt (argc, argv, "abBE")) != -1) {
+    switch (ch) {
+    case 'a':
+      res = res | OKWS_DEBUG_PUB_BINDTAB_INSERTS;
+      break;
+    case 'b':
+      res = res | OKWS_DEBUG_PUB_BINDTAB_INSERT;
+      break;
+    case 'B':
+      res = res | OKWS_DEBUG_PUB_BINDTAB_ACCESS;
+      break;
+    case 'E':
+      res = res | OKWS_DEBUG_PUB_ERRORS;
+      break;
+    default:
+      usage ();
+      break;
+    }
+  }
+
+  printf ("OKWS_DEBUG_OPTIONS=0x%qx\n", res);
+  return 0;
+}
+
