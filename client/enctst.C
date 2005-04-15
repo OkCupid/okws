efc4a5209ddcf28aadca3258acc51db0fcc8a7e8
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2005-04-15 16:46:04 +0000

    sanity checkers
    git-svn-id: svn://okws.org/trunk/okws2@748 7d287422-6eea-da11-b677-00123f2a783a

---
+++ client/enctst.C
@@ -0,0 +1,21 @@
+
+#include "cgi.h"
+
+
+int
+main (int argc, char *argv[])
+{
+  if (argc != 2)
+    exit (2);
+
+  ok_filter_cgi = XSSFILT_NONE;
+
+  str s1 = cgi_decode (argv[1]);
+  str s2 = cgi_encode (s1);
+
+  printf ("%s\n%s\n", s1.cstr (), s2.cstr ());
+  if (s2 != argv[1]) {
+	printf ("mismatch! BUG!!!\n");
+	exit (2);
+   }
+}
