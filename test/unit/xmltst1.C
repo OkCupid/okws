54a02228ec1c26d46a55b7f9496e6b26c244339b
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2006-05-29 02:11:31 +0000

    need to rename
    git-svn-id: svn://okws.org/okws2/devel/1.1@1871 7d287422-6eea-da11-b677-00123f2a783a

--- test/unit/xmltst1.C
+++ test/unit/xmltst1.C
@@ -1,8 +1,21 @@
 
 #include "okxml.h"
+#include "abuf_pipe.h"
+
+static void usage (void)
+{
+  fatalx << "usage: " << progname << "\n";
+}
 
 int
 main (int argc, char *argv[])
 {
+  setprogname (argv[0]);
+  if (argc != 1) 
+    usage ();
+
+  abuf_pipe_t src (ain);
+  abuf_t (&src);
+  
 
 }
