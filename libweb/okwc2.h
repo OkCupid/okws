8395987e3a095a1328472a13cdb963ed33cfdb00
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2006-06-21 02:34:40 +0000

    Stubs for OK Web client II
    git-svn-id: svn://okws.org/okws2/devel/1.1@2041 7d287422-6eea-da11-b677-00123f2a783a

--- libweb/okwc2.h
+++ libweb/okwc2.h
@@ -41,7 +41,12 @@
 class okwc2_req_t : public refcount {
 public:
   virtual ~okwc2_req_t () {}
-  virtual void cancel ();
+  virtual void cancel () { _c.cancel (); }
+  virtual void too_late_to_cancel () { _c.toolate (); }
+  virtual void notify_on_cancel (cbv cb) { _c.wait (cb); }
+  virtual void make (ptr<ahttpcon> x, cbi cb) { (*cb) (HTTP_UNAVAILABLE); }
+private:
+  canceller_t _c;
 };
 
 class okwc2_resp_t : public refcount {
@@ -49,14 +54,24 @@ public:
   virtual ~okwc2_resp_t () {}
 };
 
-typedef callback<void, int, ptr<okwc2_resp_t> > okwc2_cb_t;
+typedef callback<void, int, ptr<okwc2_resp_t> >::ref okwc2_cb_t;
 
 class okwc2_t : public refcount {
 public:
   okwc2_t (const str &h, int p) : _hostname (h), _port (p) {}
   virtual void req (ptr<okwc2_req_t> req, okwc2_cb_t cb) { req_T (req, cb); }
+  virtual void do_dns_request (const str &hn, cbhent cb) 
+  { do_dns_request_T (hn, cb); }
+
+  virtual void get_response (ptr<okwc2_req_t> r, ptr<ahttpcon> x, 
+			     okwc2_cb_t c) 
+  { get_response_T (r, x, c); }
 private:
   void req_T (ptr<okwc2_req_t> req, okwc2_cb_t cb, CLOSURE);
+  void do_dns_request_T (str hn, cbhent cb, CLOSURE) {}
+  void get_response_T (ptr<okwc2_req_t> r, ptr<ahttpcon> x, okwc2_cb_t cb, 
+		       CLOSURE) {}
+
   const str _hostname;
   int _port;
 };
