95e06aa74c14626558546c8585f2545a50aa0040
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2006-08-11 21:03:29 +0000

    Another patch for MW
    git-svn-id: svn://okws.org/okws2/devel/1.1@2149 7d287422-6eea-da11-b677-00123f2a783a

--- libaok/ok.h
+++ libaok/ok.h
@@ -237,6 +237,9 @@ public:
   // stuff for piecemeal output
   bool output_hdr (ssize_t sz = -1);
   bool output_fragment (str s);
+  bool output_fragment (compressible_t &b);
+  bool output_file (const char *fn, cbb::ptr cb = NULL, aarr_t *a = NULL,
+		    u_int opt = 0, penv_t *e = NULL, CLOSURE);
   bool output_done ();
 
   //
@@ -255,6 +258,7 @@ public:
   list_entry<okclnt_base_t> lnk;
 private:
   void serve_T (CLOSURE);
+  bool output_frag_prepare ();
 
 protected:
   virtual void delcb () { delete this; }
