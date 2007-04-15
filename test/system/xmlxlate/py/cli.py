d8164a36c4a6626cb92ee919f89eed4c9ce19a1b
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2007-04-15 14:51:06 +0000

    reshuffle/rename a bit; fix a bug in which we were inserting the wrong program name into the translation table
    git-svn-id: svn://okws.org/okws2/devel/1.2@2721 7d287422-6eea-da11-b677-00123f2a783a

--- test/system/xmlxlate/py/cli.py
+++ test/system/xmlxlate/py/cli.py
@@ -3,10 +3,10 @@
 import xmlrpclib
 import sys
 
-server_url = "http://0.0.0.0:8081/xlate"
+server_url = "http://0.0.0.0:8081/xlater"
 server = xmlrpclib.Server (server_url)
 server.system.setDebugLevel (10)
-res = server.xlatetst.xlate (
+res = server.xdr.xlate (
     { "hostname" : "127.0.0.1",
       "port" : 4000,
       "program" : "tst_prog_1",
