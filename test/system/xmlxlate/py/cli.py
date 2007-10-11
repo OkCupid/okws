485c3e96ccef83534e9fedba6d7bfbbc19c8ac78
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2007-10-11 19:17:10 +0000

    svn merge -r 3085:3086  svn+ssh://am.lcs.mit.edu/home/am8/max/svnrepos/okws2/devel/1.2
    git-svn-id: svn://okws.org/okws2/devel/2.0@3088 7d287422-6eea-da11-b677-00123f2a783a

--- test/system/xmlxlate/py/cli.py
+++ test/system/xmlxlate/py/cli.py
@@ -123,3 +123,27 @@ try:
 except xmlrpclib.Fault, f:
     print 'Good; caught fault: %s' % f
 
+
+for i in range(0,10):
+
+    arg = { "x" : 10,
+            "a" : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ],
+            "b" : """here is a very long long long long long string and more
+			and more stuff and then some more stuff and even then some more
+			stuff. Lots more stuff and also more and more and more stuff stuff
+			stuff. Just a few more characters to push over pkt boundary.
+			XXYYZZZ. abcdefwerwerwerwerrrweer. 21321313123939393939""",
+            "c" : "here is a shorter shorter shorter string" }
+
+    for i in range (1,6):
+        n = "this_is_a_very_long_name_%d" % i
+        arg[n] = i
+            
+ 
+    res = server.xdr.xlate (
+        { "hostname" : "127.0.0.1",
+          "port" : 4000,
+          "program" : "tst_prog_1",
+          "procno" : C.TST_RPC8,
+          "arg" : arg
+          })
