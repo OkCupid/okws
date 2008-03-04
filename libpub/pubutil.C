93ddaa26907d6036923b227feb4158e9f8369b54
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2008-03-04 00:04:47 +0000

    svn merge -r 3210:3212 svn+ssh://am.lcs.mit.edu/home/am8/max/svnrepos/okws2/devel/2.0
    git-svn-id: svn://okws.org/okws2/devel/1.2@3213 7d287422-6eea-da11-b677-00123f2a783a

--- libpub/pubutil.C
+++ libpub/pubutil.C
@@ -660,3 +660,26 @@ trunc_at_first_null (const str &s)
 
   return ret;
 }
+
+void
+fix_trailing_newlines (str *sp)
+{
+  if (sp) {
+    str s = *sp;
+    
+    if (s) {
+      const char *bp = s.cstr ();
+      const char *ep = bp + s.len ();
+      const char *nlp = ep - 1;
+      
+      for ( ; nlp >= bp && *nlp == '\n'; nlp--) ;
+      
+      // Advance to the first newline to nuke (or jump over it)
+      nlp += 2;
+      
+      if (nlp < ep) {
+        *sp = str (s.cstr (), nlp - bp);
+      }
+    }
+  }
+}
--- test/unit/cgitst1.C
+++ test/unit/cgitst1.C
@@ -2,6 +2,7 @@
 /* $Id$ */
 
 #include "okcgi.h"
+#include "pubutil.h"
 
 void
 usage ()
@@ -14,6 +15,17 @@ main (int argc, char *argv[])
 {
   if (argc != 2) 
     usage ();
+
+  str s1 =  "abcd\n\n\n\n\n\n\n";
+  str s2 =  "abcd\n";
+  str s3 =  "abcd";
+  fix_trailing_newlines (&s1);
+  fix_trailing_newlines (&s2);
+  fix_trailing_newlines (&s3);
+
+  warn << s1 << s2 << s3 ;
+
+
   ptr<cgi_t> t (cgi_t::str_parse (argv[1]));
   t->dump1 ();
 }
