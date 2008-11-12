b5628d69bb68d9bb97168e4431a20a8dca6e8e31
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2008-11-12 05:16:35 +0000

    Progress for the eve on an ssl connection object
    git-svn-id: svn://okws.org/okws2/devel/1.3@3757 7d287422-6eea-da11-b677-00123f2a783a

--- ssl/oksslcon.h
+++ ssl/oksslcon.h
@@ -3,25 +3,48 @@
 
 #ifndef __SSL_OKSSLCON_H__
 #define __SSL_OKSSLCON_H__
+
+#include "okwsconf.h"
 #ifdef HAVE_SSL
+
+#include "async.h"
+#include "tame.h"
+#include "tame_connectors.h"
+#include "tame_io.h"
+#include "tame_rendezvous.h"
+
+#include <openssl/crypto.h>
+#include <openssl/x509.h>
+#include <openssl/pem.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
 
 namespace okssl {
 
   //-----------------------------------------------------------------------
 
-  class con_t {
+  class con_t : public virtual refcount {
   public:
-    con_t (const str &hn, int port) : _hostname (hn), _port (port) {}
+    con_t (int fd, SSL *ssl);
+    ~con_t ();
+    bool ok () const { return _ok; }
+    void drain_to_network (strbuf *b, evb_t ev, CLOSURE);
+    int _fd;
   private:
-    str _hostname;
-    int _port;
+    SSL *_ssl;
+    BIO *_rbio, *_wbio;
+    bool _ok;
   };
 
   //-----------------------------------------------------------------------
 
   class factory_t {
   public:
-    ptr<con_t> alloc_con (const str &hn, int port);
+    factory_t ();
+    ~factory_t ();
+    ptr<con_t> alloc_con (int fd);
+  private:
+    SSL_CTX *_ctx;
   };
 
   ptr<factory_t> factory ();
