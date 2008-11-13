a4183f8235fc912107d8f773571d305dfb55c0f2
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2008-11-13 05:39:07 +0000

    Enough for tonight; almost all plugged in.
    git-svn-id: svn://okws.org/okws2/devel/1.3@3768 7d287422-6eea-da11-b677-00123f2a783a

--- libokssl/oksslcon.h
+++ libokssl/oksslcon.h
@@ -12,6 +12,7 @@
 #include "tame_connectors.h"
 #include "tame_io.h"
 #include "tame_rendezvous.h"
+#include "abuf.h"
 
 #include <openssl/crypto.h>
 #include <openssl/x509.h>
@@ -23,17 +24,23 @@ namespace okssl {
 
   typedef event<ssize_t>::ref evssz_t;
 
+
   //-----------------------------------------------------------------------
 
-  class con_t : public virtual refcount {
+  class con_t : public ok_xprt_base_t {
   public:
     con_t (int fd, SSL *ssl);
     ~con_t ();
     bool ok () const { return _ok; }
-    void drain_to_network (strbuf *b, evb_t ev, CLOSURE);
+    void drain_to_network (strbuf *b, evb_t ev) { drain_to_network_T (b, ev); }
     void read (char *out, size_t len, evssz_t ev, CLOSURE);
+    void drain_cancel (void);
+    abuf_src_t *alloc_abuf_src ();
     int _fd;
+  protected:
+    void drain_to_network_T (strbuf *b, evb_t ev, CLOSURE);
   private:
+    evv_t::ptr _cancel_ev;
     SSL *_ssl;
     BIO *_rbio, *_wbio;
     bool _ok;
@@ -51,6 +58,23 @@ namespace okssl {
   };
 
   ptr<factory_t> factory ();
+
+  //-----------------------------------------------------------------------
+
+  class okssl_src_t : public abuf_src_t {
+  public:
+    okssl_src_t (ptr<con_t> c) : _con (c) {}
+    ~okssl_src_t () ;
+    void init (cbv c);
+    void readcb (int n);
+    abuf_indata_t getdata ();
+    void rembytes (int n);
+    void finish () ;
+    void cancel () ;
+    bool overflow ();
+  private:
+    ptr<con_t> _con;
+  };
 
   //-----------------------------------------------------------------------
 
