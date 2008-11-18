b802a338bdf90057272fbd2980ea96bb73899907
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2008-11-18 23:51:29 +0000

    Clear up the linking confusion and circular dependencies.
    git-svn-id: svn://okws.org/okws2/devel/1.3@3805 7d287422-6eea-da11-b677-00123f2a783a

--- libahttp/ahparse.h
+++ libahttp/ahparse.h
@@ -174,29 +174,4 @@ private:
   bool _union_mode;
 };
 
-#ifdef HAVE_EXPAT
-#include "okxmlparse.h"
-class http_parser_xml_t : public http_parser_full_t {
-public:
-  http_parser_xml_t (ptr<ahttpcon> xx, int to = 0)
-    : http_parser_full_t (xx, to), _xml (&abuf) {}
-  ~http_parser_xml_t () {}
-
-  void v_cancel () ;
-  void v_parse_cb1 (int status) ;
-
-  ptr<xml_top_level_t> top_level () { return _xml.top_level (); }
-
-  ptr<const xml_top_level_t> top_level () const 
-  { return _xml.top_level (); }
-
-  str errmsg () const { return _xml.errmsg (); }
-  int errcode () const { return _xml.errcode (); }
-
-private:
-  xml_req_parser_t _xml;
-
-};
-#endif /* HAVE_EXPAT */
-
 #endif
