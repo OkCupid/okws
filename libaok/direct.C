14fec3f5ac99c79614b7ba072327bb0d381fd2ec
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2008-12-18 17:19:56 +0000

    whoops, forgot this!
    git-svn-id: svn://okws.org/okws2/devel/1.3@3850 7d287422-6eea-da11-b677-00123f2a783a

---
+++ libaok/direct.C
@@ -0,0 +1,206 @@
+#include "ok.h"
+#include "parseopt.h"
+
+//-----------------------------------------------------------------------
+
+bool
+ok_direct_ports_t::bind (const str &servpath, u_int32_t listenaddr)
+{
+  bool rc = true;
+
+  for (size_t i = 0; i < _ports.size (); i++) {
+    int port = _ports[i]._port;
+    int fd = inetsocket (SOCK_STREAM, port, listenaddr);
+    if (fd < 0) {
+      warn ("Service (%s) cannot bind to port %d: %m\n",
+	    servpath.cstr (), port);
+      rc = false;
+    } 
+    _ports[i]._fd = fd;
+  }
+  return rc;
+}
+
+//-----------------------------------------------------------------------
+
+void
+ok_direct_ports_t::init (const vec<int> &v)
+{
+  _ports.clear (); // if reiniting after a crash
+  for (size_t i = 0; i < v.size (); i++) {
+    _ports.push_back (ok_portpair_t (v[i]));
+  }
+}
+
+//-----------------------------------------------------------------------
+
+str
+ok_direct_ports_t::encode_as_str () const
+{
+  strbuf b;
+  for (size_t i = 0; i < _ports.size (); i++) {
+    str s = _ports[i].encode_as_str ();
+    if (i != 0) b << ",";
+    b << s;
+  }
+  return b;
+}
+
+//-----------------------------------------------------------------------
+
+str
+ok_portpair_t::encode_as_str () const
+{
+  strbuf b;
+  b << _port << ":" << _fd;
+  return b;
+}
+
+//-----------------------------------------------------------------------
+
+void
+ok_portpair_t::close ()
+{
+  if (_fd >= 0) {
+    ::close (_fd);
+    _fd = -1;
+  }
+}
+
+//-----------------------------------------------------------------------
+
+void
+ok_direct_ports_t::close ()
+{
+  for (size_t i = 0; i < _ports.size (); i++) { _ports[i].close (); }
+}
+
+//-----------------------------------------------------------------------
+
+bool
+ok_direct_ports_t::parse (const str &in)
+{
+  bool ret = true;
+  vec<str> v;
+  static rxx x (",");
+  split (&v, x, in);
+
+  for (size_t i = 0; i < v.size (); i++) {
+    ok_portpair_t p;
+    if (!p.parse (v[i])) {
+      warn << "Cannot parse port pair: " << v[i] << "\n";
+      ret = false;
+    } else {
+      _ports.push_back (p);
+    }
+  }
+  return ret;
+}
+
+//-----------------------------------------------------------------------
+
+bool
+ok_portpair_t::parse (const str &s)
+{
+  vec<str> v;
+  static rxx x (":");
+  split (&v, x, s);
+  return (v.size () == 2 &&
+	  convertint (v[0], &_port) &&
+	  convertint (v[1], &_fd));
+}
+
+//-----------------------------------------------------------------------
+
+void
+ok_direct_ports_t::disable_accept ()
+{
+  for (size_t i = 0; i < _ports.size (); i++) {
+    _ports[i].disable_accept ();
+  }
+}
+
+//-----------------------------------------------------------------------
+
+void
+ok_direct_ports_t::enable_accept (ok_con_acceptor_t *s, int listenq)
+{
+  for (size_t i = 0; i < _ports.size (); i++) {
+    _ports[i].enable_accept (s, listenq);
+  }
+}
+
+//-----------------------------------------------------------------------
+
+void
+ok_portpair_t::enable_accept (ok_con_acceptor_t *s, int listenq)
+{
+  if (!_listening) {
+    listen (_fd, listenq);
+    _listening = true;
+  } 
+  fdcb (_fd, selread, 
+	wrap (s, &ok_con_acceptor_t::accept_new_con, this));
+}
+
+//-----------------------------------------------------------------------
+
+void
+ok_portpair_t::disable_accept ()
+{
+  fdcb (_fd, selread, NULL);
+}
+
+//-----------------------------------------------------------------------
+
+void
+oksrvc_t::enable_direct_ports ()
+{
+  _direct_ports.enable_accept (this, ok_listen_queue_max);
+}
+
+//-----------------------------------------------------------------------
+
+void
+ok_direct_ports_t::report ()
+{
+  for (size_t i = 0; i < _ports.size (); i++) {
+    _ports[i].report ();
+  }
+}
+
+//-----------------------------------------------------------------------
+
+void
+ok_portpair_t::report ()
+{
+  warn << "++ enabled direct port " << _port << " on fd=" << _fd << "\n";
+}
+
+//-----------------------------------------------------------------------
+
+void
+oksrvc_t::disable_direct_ports ()
+{
+  _direct_ports.disable_accept ();
+}
+
+//-----------------------------------------------------------------------
+
+void
+oksrvc_t::accept_new_con (ok_portpair_t *p)
+{
+  socklen_t sinlen = sizeof (sockaddr_in);
+  sockaddr_in *sin = (sockaddr_in *)xmalloc (sinlen);
+  bzero (sin, sinlen);
+  int nfd = accept (p->_fd, reinterpret_cast<sockaddr *> (sin), &sinlen);
+  if (nfd >= 0) {
+    ptr<ahttpcon> x = ahttpcon::alloc (nfd, sin, -1, -1, false, true);
+    str n;
+    ptr<demux_data_t> d = New refcounted<demux_data_t> (p->_port, false, n);
+    ahttpcon_wrapper_t<ahttpcon> acw (x, d);
+    newclnt (acw);
+  }
+}
+
+//-----------------------------------------------------------------------
