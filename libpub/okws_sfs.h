1ff9f76248a2e775bee9e75d1c6525489e5f2955
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2009-01-17 18:11:11 +0000

    Version bumps and correct sfs versioning
    git-svn-id: svn://okws.org/okws2/devel/1.4@3951 7d287422-6eea-da11-b677-00123f2a783a

--- libpub/okws_sfs.h
+++ libpub/okws_sfs.h
@@ -31,9 +31,9 @@
 #include "str.h"
 
 #if !defined(SFSLITE_AT_VERSION) 
-# error "Need sfslite patchlevel of 1.2.4 or greater!"
+# error "Need sfslite patchlevel of 1.2.6pre1 or greater!"
 #else
-# if !SFSLITE_AT_VERSION(1,2,6,100)
+# if !SFSLITE_AT_VERSION(1,2,6,1)
 #  error "Need sfslite patchlevel of 1.2.6pre1 or greater!"
 # endif
 #endif
@@ -43,8 +43,8 @@
 // patch levels 0-99 are preleases.
 // patch level 100 is release
 #define OKWS_VERSION_MAJOR 1
-#define OKWS_VERSION_MINOR 3
-#define OKWS_VERSION_PATCHLEVEL 1
+#define OKWS_VERSION_MINOR 4
+#define OKWS_VERSION_PATCHLEVEL 0
 #define OKWS_VERSION_PRE 1
 
 #define OKWS_AT_VERSION(Maj,Min,Pat,Pre) \
