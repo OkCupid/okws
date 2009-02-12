1e366307becc666224f1cf5851f7f65167761388
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2009-02-12 16:34:21 +0000

    Export memchecker to other services and rpc servers
    git-svn-id: svn://okws.org/okws2/devel/1.4@4053 7d287422-6eea-da11-b677-00123f2a783a

---
+++ libaok/okprotutil.C
@@ -0,0 +1,39 @@
+
+#include "okprotutil.h"
+
+//-----------------------------------------------------------------------
+
+ok_xstatus_typ_t
+ok_toggle_leak_checker (ok_leak_checker_cmd_t cmd)
+{
+  ok_xstatus_typ_t status;
+
+#ifdef SIMPLE_LEAK_CHECKER
+  status = OK_STATUS_OK;
+  switch (cmd) {
+  case OK_LEAK_CHECKER_ENABLE:
+    simple_leak_checker_enable ();
+    break;
+  case OK_LEAK_CHECKER_DISABLE:
+    simple_leak_checker_disable ();
+    break;
+  case OK_LEAK_CHECKER_REPORT:
+    simple_leak_checker_report ();
+    break;
+  case OK_LEAK_CHECKER_RESET:
+    simple_leak_checker_reset ();
+    break;
+  default:
+    status = OK_STATUS_UNKNOWN_OPTION;
+    break;
+  }
+
+#else /* !SIMPLE_LEAK_CHECKER */
+  status = OK_STATUS_UNAVAIL;
+#endif /* SIMPLE_LEAK_CHECKER */
+
+  return status;
+
+}
+
+//-----------------------------------------------------------------------
---
+++ libaok/okprotutil.h
@@ -0,0 +1,11 @@
+// -*-c++-*-
+/* $Id: pubutil.h 3646 2008-09-25 20:27:26Z max $ */
+
+#ifndef _LIBAOK_OKPROTUTIL_H_
+#define _LIBAOK_OKPROTUTIL_H_ 1
+
+#include "okprot.h"
+
+ok_xstatus_typ_t ok_toggle_leak_checker (ok_leak_checker_cmd_t cmd);
+
+#endif /* _LIBAOK_OKPROTUTIL_H_ */
