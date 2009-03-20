f8aec1f6cc1ed54a5459765d23e635845b943eaf
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2009-03-20 12:15:13 +0000

    Add another library to okws for runtime functions..
    git-svn-id: svn://okws.org/okws2/devel/2.0@4218 7d287422-6eea-da11-b677-00123f2a783a

---
+++ librfn/okrfn.h
@@ -0,0 +1,24 @@
+// -*-c++-*-
+/* $Id: web.h 4029 2009-01-30 13:28:14Z max $ */
+
+#ifndef _LIBRFN_OKRFN_H_
+#define _LIBRFN_OKRFN_H_
+
+#include "pub3.h"
+
+namespace rfn1 {
+
+  //-----------------------------------------------------------------------
+
+  class std_factory_t : public pub3::rfn_factory_t {
+  public:
+
+
+  };
+
+  //-----------------------------------------------------------------------
+
+
+};
+
+#endif /* _LIBRFN_OKRFN_H_ */
---
+++ librfn/std_factory.C
@@ -0,0 +1,2 @@
+
+#include "okrfn.h"
