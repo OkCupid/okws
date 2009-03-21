13542d7dc555411b85eeb36d9ab8107147600d43
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2009-03-21 21:50:24 +0000

    svn merge -r 4238:4239 svn+ssh://am.lcs.mit.edu/home/am8/max/svnrepos/okws2/devel/1.4
    git-svn-id: svn://okws.org/okws2/devel/2.0@4240 7d287422-6eea-da11-b677-00123f2a783a

--- libahttp/httpconst.C
+++ libahttp/httpconst.C
@@ -7,10 +7,10 @@ const char *rfc_date_fmt (rfc_number_t rfc)
   const char *out = NULL;
   switch (rfc) {
   case RFC_1036:
-    out = "%A, %d-%b-%Y %T %Z";
+    out = "%A, %d-%b-%Y %T GMT";
     break;
   case RFC_1123:
-    out = "%a, %d %b %Y %H:%M:%S %Z";
+    out = "%a, %d %b %Y %H:%M:%S GMT";
     break;
   default:
     break;
