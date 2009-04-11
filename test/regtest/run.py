2849056d69b04660dbda623ac11608eeffdf6090
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2009-04-11 20:26:25 +0000

    (1) fix client/pub3 so that it can simply output JailDir and other such variables on the command line
    (2)  make a lot more progress on the regtest harness. almost
    ready to try it out.
    
    
    
    
    git-svn-id: svn://okws.org/okws2/devel/2.0@4366 7d287422-6eea-da11-b677-00123f2a783a

--- test/regtest/run.py
+++ test/regtest/run.py
@@ -1,9 +1,11 @@
-
 import os
 import time
 import sys
 import signal
 import glob
+import re
+from subprocess import Popen, PIPE
+import copy
 
 ##=======================================================================
 
@@ -35,22 +37,198 @@ def strip_ext (f, ext):
 
 class Const:
 
-    config = "test/system/okws_config"
-    exe = "okd/okld"
-    test = [ Const.exe, "-f", Const.config ]
+    okws_config = "test/system/okws_config"
+    pub_config = "test/system/pub_config"
+    
+    okld_exe = "okd/okld"
+    pub_exe = "client/pub"
+    scratch = "regtest-scratch"
+    static = "static"
+    
+    okld_test = [ okld_exe, "-f", okws_config ]
+    pub_run = [ pub_exe, "-f", pub_config ]
+
     port = 8081
-    hostaddr = "127.0.0.1"
+    hostname = "127.0.0.1"
+
+    #-----------------------------------------
 
     def __init__ (self):
+        self._jail_dir = None
         pass
+
+    #-----------------------------------------
+
+    def pub_const (self, c):
+        """Calls upon the pub v3 command-line client to lookup the value
+        of the given configuration variable."""
+
+        v = copy.copy (self.pub_run)
+        v += [ "-p", c ]
+        pipe = Popen(v, stdout=PIPE).communicate()
+        rxx = re.compile ("([^:]+):\\s+(.*)")
+        for l in pipe:
+            if l is not None:
+                m = rxx.match (l.strip ())
+                if m and m.group (1) == c:
+                    r = m.group (2)
+                    return r
+        return None
+
+    #-----------------------------------------
+
+    def jail_dir (self):
+        """Lookup the pub jail dir by calling into the pub v3 command
+        line client.  Cache the result after we're done."""
+
+        if self._jail_dir is None:
+            self._jail_dir = self.pub_const ("JailDir")
+        return self._jail_dir
+
+    #-----------------------------------------
+
+    def scratch_dir (self):
+        d = self._jail_dir
+        if d is not None:
+            d = d + "/" + self.scratch
+        return d
+
+    #-----------------------------------------
+
+    def static_url (self):
+        return self.url (self.static)
+
+    #-----------------------------------------
+
+    def service_url (self, s):
+        return self.url (s)
+
+    #-----------------------------------------
+
+    def url (self, loc):
+        return "http://%s:%d/%s" % ( self.hostname, self.port, loc)
+
+    #-----------------------------------------
+
+    def make_scratch_dir (self):
+        d = self.scratch_dir ()
+        if d is None:
+            raise RegTestError, "cannot find Pub jail or scratch dir"
+        if os.path.exists (d):
+            if !os.path.isdir (d):
+                raise RegTestError, "scratch dir (%s) is not a dir!" % d
+        else:
+            try:
+                os.mkdir (d)
+            except OSError, e:
+                raise RegTestError, e
+        return d
+
+##=======================================================================
+
+def char_subst (s, f, r):
+    out = ""
+    for c in s:
+        if c == f: c = r
+        out += c
+    return out
 
 ##=======================================================================
 
 class TestCase:
 
-    def __init__ (self, d):
+    def __init__ (self, cnst, d):
+        self._filedata = None
+        self._desc = None
+        self._outcome = None
+        self._outcome_exact = None
+        self._outcome_rxx = None
+        self._service = None
+        self._htdoc = None
+        self._const = const
+
         for k in d.keys ():
             setattr (self, "_" + k, d[k])
+
+        if not self._outcome \
+                and not self._outcome_exact \
+                and not self._outcome_rxx:
+            raise RegTestError, "bad test case: no expected outcome given"
+
+        if not self._filedata and not self._htdoc and not self._service:
+            raise RegTestError, "bad test case: no input file given"
+
+    ##----------------------------------------
+
+    def translate_data (self, in_data):
+        """Input data from Python files uses {$, $} and ${, instead
+        of the equivalent commands with '%'.  This is to prevent us
+        from doing lots of escaping when making test cases. This
+        function makes the appropriate translation back."""
+
+        rxx = re.compile ("({$|$}|${)")
+        v_in = rxx.split (in_data)
+        v_out = []
+        i = 0
+        for e in v_in:
+            if (i % 2 == 0):
+                v_out += [ e ]
+            else:
+                v_out += [ char_subst (e, '$', '%') ]
+        return ''.join (v_out)
+        
+    ##----------------------------------------
+    
+    def name (self):
+        return char_subst (self._name, '/', '-')
+
+    ##----------------------------------------
+
+    def filepath (self):
+        n = self.name ()
+        d = self._const.scratch_dir ()
+        return "%s/%s" % (d, n)
+
+    ##----------------------------------------
+
+    def file_url (self):
+
+        n = None
+        if self._filedata:
+            n = self.name ()
+        elif self._htdoc:
+            n = self._htdoc
+        
+        if n:
+            u = self._const.static_url ()
+            r =  "%s/%s" % (u, n)
+        elif self._service:
+            r = self._const.service_url (self._service)
+        else:
+            raise RegTestError, "cannot fine appropriate URL for '%s'" % \
+                self._name
+
+        return r
+
+    ##----------------------------------------
+
+    def write_data (self):
+        """Some test cases write data out to the htdocs directory, rather
+        than requiring that the data exists as part of OKWS.  This function
+        is in charge of writing that data if needs be."""
+        
+        self._const.make_scratch_dir ()
+        out = self.filepath ()
+        dat = self.translate_data (self._filedata)
+        f = fopen (out, "w")
+        f.write (dat)
+
+    ##----------------------------------------
+
+    def fetch (self):
+        u = self.file_url ()
+        return urllib.urlopen (u)
+        
 
 ##=======================================================================
 
@@ -58,8 +236,8 @@ class TestCaseLoader:
 
     ##----------------------------------------
 
-    def __init__ (self):
-        pass
+    def __init__ (self, c):
+        self._const = c
 
     ##----------------------------------------
 
@@ -79,18 +257,46 @@ class TestCaseLoader:
         except ImportError, e:
             raise RegTestError, "failed to import test case: %s", f
 
+        try:
+            v = self.load_cases (mod) 
+            if not v:
+                v = [ self.load_single_case (mod) ]
+        except RegTestError, e:
+            myerr (f, e)
+            raise RegTestError, "cannot load test case file: %s" % f
+
+        return v
+            
+    ##----------------------------------------
+
+    def load_case (self, mod):
+        """If the file has a single case, then it must have 
+        the required fields as global data fields."""
+
+        d = {}
+        rxx = re.compile ("__.*__")
+        for n in dir (mod):
+            if not rxx.match (n):
+                d[n] = getattr (mod, n)
+
+        return TestCase (self._const, d)
+
+    ##----------------------------------------
+
+    def load_cases (self, mod):
+        """If the file has specified multiple cases, then we're
+        looking for an array named 'cases' that has one test
+        case per entry."""
+
         v = []
         n = 0
-
         try:
             for c in mod.cases:
                 v["name"] = "%s.%d" (f, n)
-                v += [ TestCase (c) ]
-                n++;
+                v += [ TestCase (self._const, c) ]
+                n += 1
         except AttributeError, e:
             pass
-
-
 
         return v
 
@@ -159,8 +365,8 @@ class RegTester:
 
 def main (argv):
     c = Const ()
-    r = RegTester (c, files)
-    r.run ()
+    r = RegTester (c, argv)
+    print c.pub_const ('JailDir')
 
 ##=======================================================================
 
