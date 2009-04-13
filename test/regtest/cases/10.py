75b8b55eb9301c74797abdd81b868158faca9d5d
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2009-04-13 21:35:21 +0000

    Adding regtests is fun!  add one for regular expression matching.
    git-svn-id: svn://okws.org/okws2/devel/2.0@4391 7d287422-6eea-da11-b677-00123f2a783a

---
+++ test/regtest/cases/10.py
@@ -0,0 +1,40 @@
+#
+# Preliminary testing of regular expressions.
+#
+
+inputs = [ "aaabcccc", "abc", "aaacccc>", "abab", "aAaAbCcCcC" ]
+d = [ { "val" : s } for s in inputs ]
+
+regex = "r<a+b?c+\\>?>"
+
+#
+# for the last test case, add 0 so that the results gets coerced into
+# an int.  sneaky, i know!
+#
+filedata = """
+{$ setl { v   : %s,
+          rxx : %s } $}
+
+{$ for (i, v) {
+
+   print (i.iter + ": ")
+
+   if   (match (rxx, i.val))       { print 1 }
+   elif (match (rxx, "i", i.val))  { print 2 }
+   else                            { print 0 }
+
+   print "\\n"
+
+} $}
+
+{$ setl { x : "XXaabccc>YY" } $}
+%d: ${search(rxx,x) + 0}
+
+""" % (d, regex, len (inputs))
+
+results = [ 1, 1, 1, 0, 2, 1 ]
+
+outcome = '\n'.join (["%d: %d" % (i, results[i]) 
+                     for i in range (0, len (results))])
+
+desc = "regular expression matches"
