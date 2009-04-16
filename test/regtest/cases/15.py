1f8ab0c94ec707f2c1bf3ff404b469f47ed237ab
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2009-04-16 15:17:43 +0000

    test both relevant nestings of import. seems to work!
    git-svn-id: svn://okws.org/okws2/devel/2.0@4408 7d287422-6eea-da11-b677-00123f2a783a

--- test/regtest/cases/15.py
+++ test/regtest/cases/15.py
@@ -1,22 +1,35 @@
 
+##-----------------------------------------------------------------------
+
 rows = [ { "val_i" : i, 
            "str_i" : "str-%d-%d-str" %  (i, i) } 
          for i in range (0,20) ]
 
-filedata = [
-"""
+importline = "eval (import (row))"
+importline_pub = "{$ %s $}" % importline
+
+parent ="""
 {$ setl { rows : %s } $}
-{$ for (r, rows) {
-    include ("$[1]", { row : r })
+{$ for (row, rows) {
+    %s
+    include ("$[1]", { row : row })
 } $}
 
-""" % rows,
 """
-{$ eval (import (row));  $}
-${iter} ${val_i} ${str_i}
-"""]
 
-desc = "test the import function"
+child = "%s\n${iter} ${val_i} ${str_i}"
+
+##-----------------------------------------------------------------------
 
 outcome = '\n'.join (["%d %d %s" % (r["val_i"], r["val_i"], r["str_i"]) 
                       for r in rows ])
+
+##-----------------------------------------------------------------------
+
+cases = [ { "filedata" : [ parent % (rows, ""), child % importline_pub ],
+            "desc" : "import function, nesting A",
+            "outcome" : outcome },
+          { "filedata" : [ parent % (rows, importline), child % "" ],
+            "desc" : "import function, nesting B",
+            "outcome" : outcome } ]
+          
