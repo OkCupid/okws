7929094763735ae45ce7430b77728ae3c7002d16
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2009-06-02 01:15:48 +0000

    add a new #define for HTTP code 401.
    bump the version for good measure.
    
    
    
    
    git-svn-id: svn://okws.org/okws2/devel/2.0@4520 7d287422-6eea-da11-b677-00123f2a783a

--- libahttp/httpconst.h
+++ libahttp/httpconst.h
@@ -36,6 +36,7 @@
 #define HTTP_USEPROXY            305
 
 #define HTTP_BAD_REQUEST         400
+#define HTTP_UNAUTHORIZED        401
 #define HTTP_FORBIDDEN           403
 #define HTTP_NOT_FOUND           404
 #define HTTP_NOT_ALLOWED         405
