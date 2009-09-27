4f9f1c9a06b7cfe60e6f323f6116f149aa856de7
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2009-09-27 02:38:45 +0000

    a metainclude.
    git-svn-id: svn://okws.org/okws2/devel/3.0@4666 7d287422-6eea-da11-b677-00123f2a783a

---
+++ libpub/pub.h
@@ -0,0 +1,10 @@
+// -*-c++-*-
+/* $Id: parr.h 2784 2007-04-20 16:32:00Z max $ */
+
+#pragma once
+
+#include "pub3prot.h"
+#include "pub3ast.h"
+#include "pub3file.h"
+#include "pub3obj.h"
+#include "pub3expr.h"
