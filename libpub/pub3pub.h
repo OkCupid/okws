ecb7b2ada2730957921b58813a464eac22dbd224
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2009-10-18 19:50:08 +0000

    pub3pub factor
    git-svn-id: svn://okws.org/okws2/devel/3.0@4734 7d287422-6eea-da11-b677-00123f2a783a

---
+++ libpub/pub3pub.h
@@ -0,0 +1,19 @@
+// -*-c++-*-
+
+#pragma once
+
+#include "pub3expr.h"
+#include "pub3eval.h"
+
+namespace pub3 {
+
+  //-----------------------------------------------------------------------
+
+  class publish_t : public eval_t {
+  public:
+    void publish (str nm, ptr<bind_interface_t> d, status_ev_t ev, CLOSURE);
+  };
+
+  //--------------------------------------------------------------------
+
+};
