5650a57745f2c6216aa97a88dc43ec04d005468b
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2009-11-23 22:25:26 +0000

    Another regtest down!
    git-svn-id: svn://okws.org/okws2/devel/3.0@4916 7d287422-6eea-da11-b677-00123f2a783a

--- test/regtest/cases/3.py
+++ test/regtest/cases/3.py
@@ -1,11 +1,16 @@
-{%
+desc = "check that list modification is working properly in for loops"
+
+filedata = \
+"""{$
     for (j, [0,1]) {
        locals { v  : [ [ 1, "a" ], [ 2, "b"] , [ 3, "c" ] ] }
        for (p, v) {
           p[1] = "X" + p[1] + "x" + p[0]
         }
         for (p, v) {
-           print (p, "\n");
+           print (p[1], "\n");
         }
      }
-%}
+$}"""
+
+outcome = "Xax1 Xbx2 Xcx3 " * 2
