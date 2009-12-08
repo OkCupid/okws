0185b9a94d6e26e1b6acbe9ef24a3336c1363728
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2009-12-08 00:14:39 +0000

    last checkpoint of the night
    git-svn-id: svn://okws.org/okws2/devel/3.0@4970 7d287422-6eea-da11-b677-00123f2a783a

--- librfn/dict.C
+++ librfn/dict.C
@@ -8,144 +8,71 @@ namespace rfn1 {
 
   //-----------------------------------------------------------------------
 
-  ptr<expr_t> 
-  dict_fn_t::eval_pval (eval_t e, const pval_t *in) const
+  ptr<const expr_t>
+  values_t::v_eval_2 (publish_t *p, const vec<arg_t> &args) const
   {
-    ptr<const expr_t> x;
-    ptr<pval_t> v2;
-    ptr<expr_t> x2;
-
-    if (!in ||
-	!(x = in->to_expr ()) || 
-	!(v2 = x->eval_freeze (e)) ||
-	!(x2 = v2->to_expr ())) {
-
-      x2 = expr_null_t::alloc ();
+    ptr<expr_list_t> l = New refcounted<expr_list_t> ();
+    ptr<const expr_dict_t> d = args[0]._d;
+    bindtab_t::const_iterator_t it (*d);
+    str *key;
+    ptr<expr_t> cval;
+    while ((key = it.next (&cval))) {
+      ptr<expr_t> val;
+      if (cval) val = cval->copy ();
+      if (!val) val = expr_null_t::alloc ();
+      l->push_back (val);
     }
-
-    return x2;
+    return l;
   }
 
   //-----------------------------------------------------------------------
 
-  const nvtab_t *
-  dict_fn_t::eval_to_nvtab (eval_t e, const char *fn) const
+  ptr<const expr_t>
+  keys_t::v_eval_2 (publish_t *p, const vec<arg_t> &args) const
   {
-    const nvtab_t *nvt =  NULL;
-    ptr<const aarr_t> aa;
-    if ((aa = _arg->eval_as_dict (e))) {
-      nvt = aa->nvtab ();
-    } else {
-      report_error (e, strbuf ("%s() take a dictionary argument", fn));
+    ptr<expr_list_t> l = New refcounted<expr_list_t> ();
+    ptr<const expr_dict_t> d = args[0]._d;
+    bindtab_t::const_iterator_t it (*d);
+    str *key;
+    while ((key = it.next ())) {
+      l->push_back (expr_str_t::safe_str (*key));
     }
-    return nvt;
+    return l;
   }
 
   //-----------------------------------------------------------------------
 
-  ptr<pval_t>
-  values_t::eval_internal (eval_t e) const
+  ptr<const expr_t>
+  items_t::v_eval_2 (publish_t *p, const vec<arg_t> &args) const
   {
-    const nvtab_t *nvt = eval_to_nvtab (e, "values");
-    ptr<pval_t> ret;
-    if (nvt) {
-      ptr<expr_list_t> l = New refcounted<expr_list_t> ();
-      for (nvpair_t *p = nvt->first (); p; p = nvt->next (p)) {
-	l->push_back (eval_pval (e, p->value ()));
-      }
-      ret = l;
-    } else {
-      ret = expr_null_t::alloc ();
+    ptr<expr_list_t> l = New refcounted<expr_list_t> ();
+    ptr<const expr_dict_t> d = args[0]._d;
+    bindtab_t::const_iterator_t it (*d);
+    str *key;
+    ptr<expr_t> cval;
+    while ((key = it.next (&cval))) {
+      ptr<expr_t> val;
+      if (cval) val = cval->copy ();
+      if (!val) val = expr_null_t::alloc ();
+      ptr<expr_list_t> entry = expr_list_t::alloc ();
+      entry->push_back (expr_str_t::alloc (*key));
+      entry->push_back (val);
+      l->push_back (entry);
     }
-
-    return ret;
-  }
-
-  //-----------------------------------------------------------------------
-
-  ptr<pval_t>
-  keys_t::eval_internal (eval_t e) const
-  {
-    ptr<pval_t> ret;
-    const nvtab_t *nvt = eval_to_nvtab (e, "keys");
-    if (nvt) {
-      ptr<expr_list_t> l = New refcounted<expr_list_t> ();
-      for (nvpair_t *p = nvt->first (); p; p = nvt->next (p)) {
-	l->push_back (New refcounted<expr_str_t> (p->name ()));
-      }
-      ret = l;
-    } else {
-      ret = expr_null_t::alloc ();
-    }
-    return ret;
-  }
-
-  //-----------------------------------------------------------------------
-
-  ptr<pval_t>
-  items_t::eval_internal (eval_t e) const
-  {
-    ptr<pval_t> ret;
-    const nvtab_t *nvt = eval_to_nvtab (e, "items");
-    if (nvt) {
-      ptr<expr_list_t> l = New refcounted<expr_list_t> ();
-      for (nvpair_t *p = nvt->first (); p; p = nvt->next (p)) {
-	ptr<expr_list_t> entry = New refcounted<expr_list_t> ();
-	entry->push_back (New refcounted<expr_str_t> (p->name ()));
-	entry->push_back (eval_pval (e, p->value()));
-	l->push_back (entry);
-      }
-      ret = l;
-    } else {
-      ret = expr_null_t::alloc ();
-    }
-    return ret;
-  }
-
-  //-----------------------------------------------------------------------
-
-  json2pub_t::json2pub_t (const str &n, ptr<expr_list_t> el, int l, 
-			  ptr<expr_t> x)
-    : runtime_fn_t (n, el, l), _json (x) {}
-
-  //-----------------------------------------------------------------------
-
-  ptr<runtime_fn_t>
-  json2pub_t::constructor (const str &n, ptr<expr_list_t> e, int lineno,
-			   str *err)
-  {
-    ptr<runtime_fn_t> ret;
-    size_t narg = e ? e->size () : size_t (0);
-
-    if (narg != 1) {
-      *err = "json2pub() takes one argument (a string)";
-    } else {
-      ret = New refcounted<json2pub_t> (n, e, lineno, (*e)[0]);
-    }
-    return ret;
+    return l;
   }
 
   //-----------------------------------------------------------------------
   
-  ptr<expr_t> 
-  json2pub_t::eval_internal (eval_t e) const
+  ptr<const expr_t>
+  json2pub_t::v_eval_2 (publish_t *p, const vec<arg_t> &args) const
   {
+    str s = args[0]._s;
     ptr<expr_t> ret;
-    str s = _json->eval_as_str (e);
-    if (s) { ret = json_parser_t::parse (s); }
-    if (!ret) ret = expr_null_t::alloc ();
-    return ret;
+    json_parser_t parser;
+    ret = parser.parse (s);
+    return expr_str_t::safe_alloc (ret);
   }
-
-  //-----------------------------------------------------------------------
-
-  ptr<pval_t> json2pub_t::eval_freeze (eval_t e) const 
-  { return eval_internal (e); }
-
-  //-----------------------------------------------------------------------
-
-  ptr<const pval_t> json2pub_t::eval (eval_t e) const 
-  { return eval_internal (e); }
 
   //-----------------------------------------------------------------------
 
--- librfn/regex.C
+++ librfn/regex.C
@@ -4,67 +4,22 @@ namespace rfn1 {
 
   //-----------------------------------------------------------------------
 
-  rxx_fn_t::rxx_fn_t (const str &n, ptr<expr_list_t> l, int ln,
-		      ptr<expr_t> x, ptr<expr_t> val, bool match)
-    : predicate_t (n, l, ln),
-      _rxx (x),
-      _val (val),
-      _match (match) {}
-
-  //-----------------------------------------------------------------------
-
-  ptr<runtime_fn_t>
-  rxx_fn_t::constructor (const str &n, ptr<expr_list_t> e, int lineno, str *err)
+  ptr<const expr_t>
+  match_t::v_eval_2 (publish_t *p, const vec<arg_t> &args) const
   {
-    ptr<runtime_fn_t> ret;
-    size_t narg = e ? e->size () : size_t (0);
-    bool ok = true;
-    ptr<expr_t> regex, val;
-    bool match;
-
-    match = (n == "match");
-
-    if (narg == 3) {
-
-      ptr<expr_list_t> l = New refcounted<expr_list_t> (lineno);
-      l->push_back ((*e)[0]);
-      l->push_back ((*e)[1]);
-      regex = l;
-
-      val = (*e)[2];
-    } else if (narg == 2) {
-      regex = (*e)[0];
-      val = (*e)[1];
-    } else {
-      *err = "match/search take 2 or 3 arguments";
-      ok = false;
-    }
-
-    if (ok) {
-      ret = New refcounted<rxx_fn_t> (n, e, lineno, regex, val, match);
-    }
-    return ret;
+    ptr<rxx> x = args[0]._r;
+    str s = args[1]._s;
+    return x->match (s);
   }
 
   //-----------------------------------------------------------------------
 
-  bool
-  rxx_fn_t::eval_internal_bool (eval_t e) const
+  ptr<const expr_t>
+  search_t::v_eval_2 (publish_t *p, const vec<arg_t> &args) const
   {
-    ptr<rxx> x;
-    str v;
-    bool ret = false;
-
-    if (!(x = _rxx->eval_as_regex (e))) {
-      report_error (e, "cannot evaluate regex in match/search");
-    } else if (!_val || !(v = _val->eval_as_str (e))) {
-      /* noop -- false */
-    } else if (_match) {
-      ret = x->match (v);
-    } else {
-      ret = x->search (v);
-    }
-    return ret;
+    ptr<rxx> x = args[0]._r;
+    str s = args[1]._s;
+    return x->search (s);
   }
 
   //-----------------------------------------------------------------------
