d97a20adf207bbdfd85fda0dc068aaed0b6fefaf
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2009-12-20 05:25:01 +0000

    another regtest in place
    git-svn-id: svn://okws.org/okws2/devel/3.0@5018 7d287422-6eea-da11-b677-00123f2a783a

--- test/regtest/cases/37.py
+++ test/regtest/cases/37.py
@@ -2,9 +2,9 @@ desc = "test hidden escape"
 
 filedata = """
 {$
-   setl { text : 'foo <bar> "hi" & \\'bye\\'' }
+   locals { text : 'foo <bar> "hi" & \\'bye\\'' }
 $}
-${text|hidden}
+${text|hidden_escape}
 """
 
 outcome = "foo &lt;bar&gt; &quot;hi&quot; &amp; &#039;bye&#039;"
