dececdc1e7223dc1761f56e2178995802c22f30e
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2009-12-21 20:54:44 +0000

    these are the last two regression tests from the old suite.  success!
    git-svn-id: svn://okws.org/okws2/devel/3.0@5036 7d287422-6eea-da11-b677-00123f2a783a

--- test/regtest/cases/59.py
+++ test/regtest/cases/59.py
@@ -9,31 +9,24 @@
 #-----------------------------------------------------------------------
 
 dct = { "v" : "var v",
-        "js" : 'javascript:$("foo").bar',
-        "exp" : "vv ${v} ${js} vv" }
+        "js" : 'javascript:$("foo").bar' }
 
 parent1 = """ {$ include ("$[1]", %s) $} """  % dct
 parent2 = """ 
-{$ setl { file : "$[1]" } $}
+{$ locals { file : "$[1]" } $}
 {$ include (file, %s) $}
 """ % dct
 
 child = """
 ${v}
-$${v}
 ${js}
-$${js}
-$${exp}
 """ 
 
 outcome ="""
 %s
 %s
-%s
-%s
-vv %s %s vv""" % (dct["v"], dct["v"],
-                  dct["js"], dct["js"],
-                  dct["v"], dct["js"])
+""" % (dct["v"],
+       dct["js"])
 
 cases = [ { "filedata" : [ parent1, child ],
             "outcome" : outcome,
--- test/regtest/cases/60.py
+++ test/regtest/cases/60.py
@@ -13,27 +13,17 @@ def filedata_fn (func):
 {$ %s { x : "derived x: ${x}; ${y}",
         y : "derived y: ${y}; ${x}" } $}
 ${x}
-$${x}
 ${y}
-$${y}
 """ % (func, func)
 
-outcomes = { "set" : """
+outcome = """
 derived x: base x; base y
-derived x: base x; base y
-derived y: base y; base x
-derived y: base y; base x
-""", 
-             "setl" : """
-derived x: base x; base y
-derived x: base x; base y
-derived y: base y; base x
-derived y: base y; base x
-""" } 
+derived y: base y; derived x: base x; base y
+"""
 
 desc_fmt = "shell string expansion (via %s)"
 
 cases = [ { "filedata" : filedata_fn (f),
             "desc" : desc_fmt % f,
-            "outcome" : outcomes[f] } for f in [ "set", "setl" ] ]
+            "outcome" : outcome } for f in [ "globals", "locals" ] ]
 
