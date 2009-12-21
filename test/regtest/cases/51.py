1f7e3e9f94f67fa18e0464341b229a781efcf62a
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2009-12-21 15:24:29 +0000

    more regtest happiness
    git-svn-id: svn://okws.org/okws2/devel/3.0@5029 7d287422-6eea-da11-b677-00123f2a783a

---
+++ test/regtest/cases/51.py
@@ -0,0 +1,10 @@
+
+desc = "test of server-side objects (31.py from OKWS/2.1)"
+
+script_path = "objtest"
+
+outcome = "1 40 0 hi " + \
+"""o = {"badass" : [{"lion" : [null, null, {"giraffe" : "moo"}], "cat" : "meow", "dog" : "DOG-#-0"}, 
+                    {"lion" : [null, null, {"giraffe" : "moo"}], "cat" : "meow", "dog" : "DOG-#-1"}, 
+                    {"lion" : [null, null, {"giraffe" : "moo"}], "cat" : "meow", "dog" : "DOG-#-2"}], 
+         "y" : [null, null, 40], "x" : [null, null, null, "hi"], "z" : 1}"""
--- test/regtest/cases/v-2.1/32.py
+++
@@ -1,6 +0,0 @@
-
-desc = "test of server-side objects"
-
-script_path = "objtest"
-
-outcome = "1 40 0 hi"
