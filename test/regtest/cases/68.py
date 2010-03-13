72e9078704f711e2ef8241d5e48437894b004ae4
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2010-03-13 16:40:46 +0000

    Super-awesome test of the configuration system, as we're likely to see it called on the live OkCupid site.
    git-svn-id: svn://okws.org/okws2/devel/3.0@5147 7d287422-6eea-da11-b677-00123f2a783a

---
+++ test/regtest/cases/68.py
@@ -0,0 +1,62 @@
+##-----------------------------------------------------------------------
+
+desc = "test configuration file inclusion, nested + sequential"
+
+##-----------------------------------------------------------------------
+
+filedata= [ 
+"""
+{$ 
+   globals { switch_on : false }
+   include ("$[1]");
+   if (switch_on) {
+       globals { 
+            bar : 20,
+            jam : "hello"
+       }
+       universals {
+            jazz : "all"
+       }
+   }
+   universals { "pickle" : "half-sour" }
+   switch_on = false;
+$}
+""", 
+"""
+{$
+   switch_on = true;
+$}
+""",
+"""
+{$
+   universals { blah : "blahblah" }
+   if (jam) {
+       globals { "whoop" : 100 }
+   }
+   locals { "jazz" : false }
+   if (!jazz) {
+      pickle = "dill";
+   }
+$}
+""" ]
+
+##-----------------------------------------------------------------------
+
+display = [ ("switch_on", "false"),
+            ("bar", 20),
+            ("jam", "hello"),
+            ("jazz", "all"),
+            ("blah" , "blahblah"),
+            ("whoop", 100 ),
+            ("pickle", "dill") ]
+
+##-----------------------------------------------------------------------
+
+script_path = "configtest?global=1&fn=$[0]&fn=$[2]&display=" + \
+    ','.join ([p[0] for p in display])
+
+##-----------------------------------------------------------------------
+
+outcome= "\n".join (["%s -> %s" % p for p in display ])
+
+##-----------------------------------------------------------------------
