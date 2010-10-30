7003bd58091beb500039086bb2f4cce63d0e35e3
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2010-10-30 23:09:40 +0000

    behold a very simple jsonrpc class.. all we need
    git-svn-id: svn://okws.org/okws2/devel/3.1@5476 7d287422-6eea-da11-b677-00123f2a783a

---
+++ py/jsonrpc.py
@@ -0,0 +1,106 @@
+"""
+   jsonrpc
+
+   A class that communicates over SUN-RPC but does marshalling via
+   JSON-encoding, rather than XDR.
+"""
+
+import random
+import socket
+import struct
+import json
+
+##-----------------------------------------------------------------------
+
+class Error (Exception):
+    def __init__ (self, value):
+        self._err = value
+    def __str__ (self):
+        return repr (self._err)
+
+##-----------------------------------------------------------------------
+
+class Client:
+
+    #-----------------------------------------
+
+    def __init__ (self, fd = -1, prog = None, vers = None, 
+                  host = None, port = -1):
+        self._fd = fd
+        self._prog = prog
+        self._vers = vers
+        self._host = host
+        self._port = port
+        self._socket = None
+
+    #-----------------------------------------
+    
+    def connect (self, host = None, port = -1):
+        if host: self._host = host
+        if port >= 0: self._port = port
+        self._socket = socket.create_connection ((self._host, self._port))
+
+    #-----------------------------------------
+
+    def make_packet (self, proc, arg):
+
+        # add the HEADER longs as follows:
+        xid = random.randint(0,0xffffffff)
+        call = 0
+        js = 3
+        hdr = struct.pack (">" + "L" * 6, xid, call, js, 
+                           self._prog, self._vers, proc)
+
+        # make an XDR "Auth" Field
+        hdr += struct.pack ("x" * 4 * 4) 
+
+        # put in the arg payload
+        packet_inner = hdr + json.dumps (arg)
+
+        # pad to a multiple of 4 bytes
+        plen = len (packet_inner)
+        rem = plen % 4
+        if rem != 0:
+            packet_inner += struct.pack ("x" * (4 - rem))
+
+        # prepend the packet length
+        encoded_len = len (packet_inner) | 0x80000000;
+        packet = struct.pack (">L", encoded_len) + packet_inner
+
+        return (packet, xid)
+
+    #-----------------------------------------
+
+    def receive_packet (self, xid):
+
+        # get the packet len, and decode via bitwise AND
+        packlen_raw = self._socket.recv (4)
+        (packlen,) = struct.unpack (">L", packlen_raw)
+        packlen = packlen & 0x7fffffff
+        packet = self._socket.recv (packlen)
+
+        (xid_in, reply) = struct.unpack (">LL", packet[0:8])
+        if xid_in != xid:
+            raise Error, "bad xid (%d != %d)" % (xid, xid_in)
+        if reply != 1:
+            raise Error, "expected reply bit, got %d" % reply
+        l = len (packet)
+        start = 24
+
+        if l < start:
+            raise Error, "packet too small, expected at least 24b, got %d" % l
+
+        if l == start:
+            ret = None
+        else:
+            res = str (packet[start:])
+
+        return res
+
+    #-----------------------------------------
+
+    def call (self, proc, arg):
+       
+        (packet, xid) = self.make_packet (proc, arg)
+        self._socket.send (packet)
+        return self.receive_packet (xid)
