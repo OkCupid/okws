686c5f8f53225754d92d1cbf7b89d989c98b55ca
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2010-10-31 17:22:12 +0000

    full round trip test for json rpc!
    git-svn-id: svn://okws.org/okws2/devel/3.1@5477 7d287422-6eea-da11-b677-00123f2a783a

--- test/system/py/xdrjson.py
+++ test/system/py/xdrjson.py
@@ -1,49 +1,28 @@
 
-import struct
-import json
-import socket
+import time
+import sys
 
+sys.path += [ "../../../py" ]
 
-s = socket.create_connection (("127.0.0.1", 10808))
+import jsonrpc
 
-xid = 87717
-call = 0
-js = 3
-prog = 10808
-vers = 1
-proc = 1
-hdr = struct.pack (">" + "L" * 6, xid, call, js, prog, vers, proc);
+cli = jsonrpc.Client (host = "127.0.0.1", port = 10808,
+                      prog = 10808, vers = 1)
 
-# make an XDR "Auth" Field
-hdr += struct.pack ("x" * 4 * 4) 
+cli.connect ()
+
+serial = str (int (time.time ()) % 10000)
+
+key = "test-key-" + str (int (time.time ()) % 10000)
 
 date = { "date" : { "on" : True, 
                     "date" : { "mday" : 27, "mon" : 11, "year" : 1977 } },
          "time" : { "on" : False} }
-data = { "d" : date, "i" : 3000, "pk" : 4004, "d2" : date}
-put_arg = { "key" : "test-key-1",
+data = { "d" : date, "i" : serial , "pk" : 4008, "d2" : date}
+put_arg = { "key" : key,
             "data" : data }
 
-packet = hdr + json.dumps (put_arg)
-plen = len(packet)
-rem = plen % 4
-if rem != 0:
-    packet += struct.pack ("x" * (4 - rem))
+print cli.call (1, put_arg)
 
-full_packet = struct.pack (">L", len (packet) + 0x80000000) + packet
+print cli.call (2, key)
 
-print full_packet
-s.send (full_packet)
-
-packlen_struct = s.recv (4)
-print (len (packlen_struct))
-print packlen_struct
-packlen = struct.unpack (">L", packlen_struct)
-packlen = packlen[0] & 0x7ffffff
-print packlen
-stuff = s.recv (packlen)
-(xid,reply) = struct.unpack(">LL", stuff[0:8])
-stuff = stuff[24:]
-print "XID: " + str (xid)
-print "Reply bit: " + str (reply)
-print stuff
