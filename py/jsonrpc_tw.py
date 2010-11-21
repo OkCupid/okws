43f1a49c0de88f8528f8491566d3d4ad272d0b8d
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2010-11-21 15:43:03 +0000

    trying to figure out this twisted stuff, i think i've made some progress
    git-svn-id: svn://okws.org/okws2/devel/3.1@5522 7d287422-6eea-da11-b677-00123f2a783a

--- py/jsonrpc_tw.py
+++ py/jsonrpc_tw.py
@@ -7,22 +7,147 @@ from twisted.application import internet, service
 from twisted.internet import defer, protocol, reactor
 from twisted.protocols import basic
 from twisted.python import components
+from twisted.internet.defer import inlineCallbacks, Deferred
+import sys
 
 ##-----------------------------------------------------------------------
 
-class Axprt (protocol.Protocol):
+def sleep (secs):
+    d = Deferred()
+    reactor.callLater(secs, d.callback, None)
+    return d
+
+##-----------------------------------------------------------------------
+
+class AxprtConn (protocol.Protocol):
+    def __init__ (self):
+        pass
     def connectionMade (self):
-        pass
+        self.factory.associate (self)
+        self.transport.write ("yo pimp daddy!")
     def dataReceived (self, data):
-        pass
-    def connectoinLost (self, reason):
+        print "got data: " + data
+    def connectionLost (self, reason):
         pass
     
 ##-----------------------------------------------------------------------
 
-class AxprtFactory (protocol.ClientFactory):
+def info (s):
+        print "INFO: " + s
 
-    protocol = Axprt
+##-----------------------------------------------------------------------
 
-    def __init__ (self):
-        pass
+def warn (s):
+    print "WARN: " + s
+ 
+##-----------------------------------------------------------------------
+
+class Axprt (protocol.ClientFactory):
+
+    protocol = AxprtConn
+
+    ##------------------------------
+
+    def __str__ (self):
+        return "%s:%d" % (self._host, self._port)
+
+    ##------------------------------
+
+    def info (self, s):
+        if self._verbose:
+            info (str (self) + ": " + s)
+
+    ##------------------------------
+
+    def warn (self, s):
+        warn (str (self) + ": " + s)
+
+    ##------------------------------
+
+    def __init__ (self, host, port, timeout = 2, verbose = False):
+        self._host = host
+        self._port = port
+        self._timeout = timeout
+        self._verbose = verbose
+        self._first_cb = None
+        self._conn = None
+
+    ##------------------------------
+
+    def clientConnectionFailed(self, connector, reason):
+        self.warn ("connection failed")
+        self._retry ()
+    
+    ##------------------------------
+
+    def clientConnectionLost(self, connector, reason):
+        self.warn ("connection closed")
+        self._retry ()
+
+    ##------------------------------
+
+    def _launch (self):
+        self.info ("launch")
+        reactor.connectTCP(self._host, self._port, self)
+        
+    ##------------------------------
+
+    @inlineCallbacks
+    def _retry (self):
+        if not self._first (False):
+            self.info ("sleep %d" % self._timeout)
+            yield sleep (self._timeout)
+            self._launch ()
+
+    ##------------------------------
+
+    def _first (self, rc):
+        ret = False
+        if self._first_cb:
+            c = self._first_cb
+            self._first_cb = None
+            c.callback (rc)
+            ret = True
+        return ret
+
+    ##------------------------------
+
+    def associate (self, x):
+        self._conn = x
+        self._first (True)
+
+    ##------------------------------
+
+    def connect (self, first_retry = True):
+        """Connect an Axprt to the given host and port. Return a deferred
+        that's fired after the first connection happens.  If the first
+        connection fails, error out."""
+        cb = Deferred ()
+        if first_retry:
+            self._first_cb = cb
+        else:
+            cb.callback (True)
+        self._launch ()
+        return cb
+
+##-----------------------------------------------------------------------
+
+@inlineCallbacks
+def do_test ():
+    x = Axprt (host = "localhost", port = 8000, verbose = True)
+    ok = yield x.connect ()
+    if ok:
+        print "connection happened!"
+    else:
+        print "failure!"
+        reactor.stop ()
+
+##-----------------------------------------------------------------------
+
+def main ():
+    do_test()
+    reactor.run()
+
+# this only runs if the module was *not* imported
+if __name__ == '__main__':
+    main()
