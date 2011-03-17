cc7ab33a73affbc5fbdb1e1a88cb528da91579de
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2011-03-17 17:33:01 +0000

    one more version bump to 3.1.6
    git-svn-id: svn://okws.org/okws2/devel/3.1@5643 7d287422-6eea-da11-b677-00123f2a783a

--- libpub/okws_sfs.h
+++ libpub/okws_sfs.h
@@ -51,8 +51,8 @@
 // patch level 100 is release
 #define OKWS_VERSION_MAJOR 3
 #define OKWS_VERSION_MINOR 1
-#define OKWS_VERSION_PATCHLEVEL 5
-#define OKWS_VERSION_PRE 106
+#define OKWS_VERSION_PATCHLEVEL 6
+#define OKWS_VERSION_PRE 100
 
 #define OKWS_AT_VERSION(Maj,Min,Pat,Pre) \
   (VERSION_FLATTEN(Maj,Min,Pat,Pre) <= \
