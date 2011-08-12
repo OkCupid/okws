ff007cee5b4d1a1c7833c0e31742a38ebdcc51eb
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2011-08-12 22:03:47 +0000

    a bit more hacketry
    git-svn-id: svn://okws.org/okws2/devel/3.1@5745 7d287422-6eea-da11-b677-00123f2a783a

--- libpub/pub3msgpackrpc.h
+++ libpub/pub3msgpackrpc.h
@@ -9,15 +9,36 @@ namespace pub3 {
 
   namespace msgpack {
 
+    enum {
+      REQUEST  = 0, //  [0, msgid, method, param]
+      RESPONSE = 1, //  [1, msgid, error, result]
+      NOTIFY = 2   //  [2, method, param]
+    };
+
     //========================================
 
     typedef event<int, ptr<expr_t> >::ref ev_t;
+    typedef event<int, ptr<expr_t>, ptr<expr_t> >::ref callev_t;
+
+    //========================================
+
+    class dependent_t {
+    public:
+      virtual void eof_hook (int rc) = 0;
+      list_entry<dependent_t> _lnk;
+    };
+
+    //========================================
 
     class axprt : public virtual refcount {
     private:
       int _errno;
       int _fd;
       strbuf _inbuf;
+      u_int32_t _seqid;
+
+      qhash<u_int32_t, callev_t::ptr> _calls;
+
       axprt (int fd);
       tame::lock_t _write_lock, _read_lock;
       void close (int en);
@@ -25,13 +46,35 @@ namespace pub3 {
       bool is_open () const { return _fd >= 0; }
       str get_str (size_t bytes) const;
       friend class refcounted<axprt>;
+
+      u_int32_t seqid () ;
+
     public:
       static ptr<axprt> alloc (int fd);
       void send (ptr<const expr_t> x, evi_t ev, CLOSURE);
       void recv (ev_t ev, CLOSURE);
+      void dispatch (CLOSURE);
+      void call (str mthd, ptr<const expr_t> arg, callev_t ev, CLOSURE);
+      
       ~axprt ();
     };
     
+    //========================================
+
+    class aclnt : public virtual refcount {
+    private:
+      ptr<axprt> _x;
+      str _prog;
+      aclnt (ptr<axprt> x, str prog);
+      ~aclnt ();
+      friend class refcounted<aclnt>;
+
+    public:
+      void call (str method, ptr<const expr_t> arg, ptr<expr_t> *res,
+		 aclnt_cb cb, CLOSURE);
+      static ptr<aclnt> alloc (ptr<axprt> x, str prog);
+    };
+
     //========================================
     
   }
