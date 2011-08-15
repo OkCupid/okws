bdefa79dfd18176be0bd2d7ebad368a2394eb393
max <max@7d287422-6eea-da11-b677-00123f2a783a>
2011-08-15 22:44:29 +0000

    a bit more hacking.
    git-svn-id: svn://okws.org/okws2/devel/3.1@5747 7d287422-6eea-da11-b677-00123f2a783a

--- libpub/pub3msgpackrpc.h
+++ libpub/pub3msgpackrpc.h
@@ -4,6 +4,7 @@
 
 #include "async.h"
 #include "pub3.h"
+#include "tame_rendezvous.h"
 
 namespace pub3 {
 
@@ -40,38 +41,55 @@ namespace pub3 {
 
     //========================================
 
-    class axprt : public virtual refcount {
+    class axprt_inner : public virtual refcount {
     private:
       int _errno;
       int _fd;
       strbuf _inbuf;
       u_int32_t _seqid;
       str _remote;
+      tame::lock_t _read_lock, _write_lock;
+      evv_t::ptr _read_stop, _write_stop;
+      rendezvous_t<> _read_rv, _write_rv;
 
       qhash<u_int32_t, callev_t::ptr> _calls;
 
-      axprt (int fd);
-      tame::lock_t _write_lock, _read_lock;
+      axprt_inner (int fd);
       void close (int en);
       void recv_json (size_t lim, size_t ps, ev_t ev, CLOSURE);
       bool is_open () const { return _fd >= 0; }
       str get_str (size_t bytes) const;
-      friend class refcounted<axprt>;
+      friend class refcounted<axprt_inner>;
 
       u_int32_t seqid () ;
       void error (str msg);
       str get_remote ();
 
     public:
-      static ptr<axprt> alloc (int fd);
+      void stop ();
+      static ptr<axprt_inner> alloc (int fd);
       void send (ptr<const expr_t> x, evb_t ev, CLOSURE);
       void recv (ev_t ev, CLOSURE);
       void dispatch (CLOSURE);
       void call (str mthd, ptr<const expr_t> arg, callev_t::ptr ev, CLOSURE);
       
-      ~axprt ();
+      ~axprt_inner ();
     };
     
+    //========================================
+
+    class axprt : public virtual refcount {
+      ptr<axprt_inner> _xi;
+      axprt (int fd);
+      friend class refcounted<axprt>;
+      void run ();
+      
+    public:
+      void call (str mthd, ptr<const expr_t> arg, callev_t::ptr ev);
+      static ptr<axprt> alloc (int fd);
+      ~axprt ();
+    };
+
     //========================================
 
     class aclnt : public virtual refcount {
