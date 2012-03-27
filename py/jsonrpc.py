82974c55364db00182ee8962fe44765aaf6dba2d
Tom Jacques <tj@okcupid.com>
2012-03-27 11:06:41 -0400

    simplejson jsonprc change for okws

--- py/jsonrpc.py
+++ py/jsonrpc.py
@@ -8,7 +8,8 @@
 import random
 import socket
 import struct
-import json
+try: import simplejson as json
+except ImportError: import json
 import time
 
 ##-----------------------------------------------------------------------
